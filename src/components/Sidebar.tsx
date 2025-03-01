40258c748d259f113f4bc8b207f7a02e296e9922
Amit Agarwal <amit.agarwal@global.ntt>
2025-03-01 18:47:21 +0530

    Intermediate Commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { MENU_LINKS } from "@/constants";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -11,8 +12,10 @@ const Sidebar = () => {
     <section className="sticky left-0 top-0 flex-container h-screen w-fit flex-col justify-between bg-sidebar p-6 pt-28 text-sidebar-primary max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {MENU_LINKS.map((menu) => {
+          console.log(pathName);
           const isActive =
-            pathName === menu.route || pathName.startsWith(menu.route);
+            pathName === menu.route || pathName.startsWith(`${menu.route}/`);
+          console.log(isActive);
           return (
             <Link
               href={menu.route}
@@ -20,12 +23,20 @@ const Sidebar = () => {
               className={cn(
                 "flex gap-4 items-center p-4 rounded-l-lg justify-start",
                 {
-                  "text-sidebar-primary-foreground bg-sidebar-primary":
+                  "bg-sidebar-primary text-sidebar-primary-foreground":
                     isActive,
                 }
               )}
             >
-              {menu.label}
+              <Image
+                src={menu.imgUrl}
+                alt={menu.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-xl font-semibold max-lg:hidden">
+                {menu.label}
+              </p>
             </Link>
           );
         })}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,27 +1,27 @@
 export const MENU_LINKS = [
   {
     label: "Home",
-    imgUrl: "",
+    imgUrl: "/icons/home.gif",
     route: "/",
   },
   {
     label: "Upcoming Meetings",
-    imgUrl: "",
+    imgUrl: "/icons/tomorrow.png",
     route: "/upcoming",
   },
   {
     label: "Previous Meetings",
-    imgUrl: "",
+    imgUrl: "/icons/yesterday.png",
     route: "/previous",
   },
   {
     label: "Recordings",
-    imgUrl: "",
+    imgUrl: "/icons/recording.gif",
     route: "/recordings",
   },
   {
     label: "Personal Room",
-    imgUrl: "",
+    imgUrl: "/icons/personal.gif",
     route: "/personal-room",
   },
 ];
